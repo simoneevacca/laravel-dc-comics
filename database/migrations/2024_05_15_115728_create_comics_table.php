b106903c2a2c13f06a1dfd53cff0b9d8c055b639
simoneevacca <v.simone.vacca@gmail.com>
2024-05-15 14:05:23 +0200

    first commit

---
+++ database/migrations/2024_05_15_115728_create_comics_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 100);
+            $table->text('description');
+            $table->string('thumb', 255);
+            $table->string('price', 10);
+            $table->string('series', 40);
+            $table->string('sale_date', 15);
+            $table->string('type', 20);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comics');
+    }
+};
---
+++ resources/views/home.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.app')
+@section('content')
+    <h1>home</h1>
+@endsection
