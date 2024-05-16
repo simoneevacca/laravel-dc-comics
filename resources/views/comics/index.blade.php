ffeeb817d4f8f025e6d65f1c56c70b5c779dc0d4
simoneevacca <v.simone.vacca@gmail.com>
2024-05-16 14:01:27 +0200

    add modale before deleting comic

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <div class="container">
         <h1 class="my-5">Tutti i fumetti</h1>
-        <a href="{{ route('comics.create') }}" class="btn btn-primary mb-4" >
+        <a href="{{ route('comics.create') }}" class="btn btn-primary mb-4">
             Add new
         </a>
 
@@ -32,11 +32,55 @@
                             <td>
                                 <a href="{{ route('comics.show', $comic) }}">view</a>
                                 <a href="{{ route('comics.edit', $comic) }}">edit</a>
-                                <form action="{{ route('comics.destroy', $comic) }}" method="post">
-                                    @csrf
-                                    @method('DELETE')
-                                    <input type="submit" value="delete">
-                                </form>
+
+                                <!-- Modal trigger button -->
+                                <button type="button" class="btn btn-primary btn-sm" data-bs-toggle="modal"
+                                    data-bs-target="#modalId-{{ $comic->id }}">
+                                    delete
+                                </button>
+
+                                <!-- Modal Body -->
+                                <!-- if you want to close by clicking outside the modal, delete the last endpoint:data-bs-backdrop and data-bs-keyboard -->
+                                <div class="modal fade" id="modalId-{{ $comic->id }}" tabindex="-1" data-bs-backdrop="static"
+                                    data-bs-keyboard="false" role="dialog" aria-labelledby="modalTitleId-{{ $comic->id }}"
+                                    aria-hidden="true">
+                                    <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-sm"
+                                        role="document">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title" id="modalTitleId-{{ $comic->id }}">
+                                                    Eliminazione fumetto
+                                                </h5>
+                                                <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                    aria-label="Close"></button>
+                                            </div>
+                                            <div class="modal-body">⚠️Sei sicuro di voler eliminare questo fumetto?⚠️</div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+                                                    Annulla
+                                                </button>                                           
+                                                <form action="{{ route('comics.destroy', $comic) }}" method="post">
+                                                    @csrf
+                                                    @method('DELETE')
+                                                    <button type="submit" class="btn btn-danger">Conferma</button>
+                                                </form>                                            
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <!-- Optional: Place to the bottom of scripts -->
+                                <script>
+                                    const myModal = new bootstrap.Modal(
+                                        document.getElementById("modalId"),
+                                        options,
+                                    );
+                                </script>
+
+
+
+                               
                             </td>
 
 
